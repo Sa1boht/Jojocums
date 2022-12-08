dfe179a051991b2e0a7ec8938185f06dac1605c3
Sa1boht <thobias.andreatta@gmail.com>
2022-12-08 15:30:09 -0300

    Adicionando models à pagina de administração

---
+++ Jojocums/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(Avaliacao)
+admin.site.register(Genero)
+admin.site.register(Produtora)
+admin.site.register(Distribuidora)
+admin.site.register(Jogo)

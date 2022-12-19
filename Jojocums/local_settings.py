aa9334b9d3903f09ce28e6d57faba7a99bc621ed
Sa1boht <thobias.andreatta@gmail.com>
2022-12-19 18:46:24 -0300

    Settings

--- Jojocums/local_settings.py
+++ Jojocums/local_settings.py
@@ -2,12 +2,12 @@
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'jojocums',
-        'USER': 'sa1boht',
-        'PASSWORD': 'senha123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "jojocums",
+        "USER": "sa1boht",
+        "PASSWORD": "senha123",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
--- Jojocums/settings.py
+++ Jojocums/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "Jojocums",
 ]
 
 MIDDLEWARE = [
@@ -72,20 +73,16 @@ WSGI_APPLICATION = "Jojocums.wsgi.application"
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME":
-        "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        "NAME":
-        "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        "NAME":
-        "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        "NAME":
-        "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 

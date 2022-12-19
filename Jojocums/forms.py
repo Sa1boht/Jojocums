35e02dd80c3dab0286968597723c5e921a846e98
Sa1boht <thobias.andreatta@gmail.com>
2022-12-19 19:18:01 -0300

    Adicionando form de avaliação

--- Jojocums/forms.py
+++ Jojocums/forms.py
@@ -1,8 +1,20 @@
 from django import forms
-from .models import Jogo
+from .models import Jogo, Avaliacao
 
 
 class JogoForm(forms.ModelForm):
     class Meta:
         model = Jogo
         fields = "__all__"
+
+
+class AvaliacaoForm(forms.ModelForm):
+    class Meta:
+        model = Avaliacao
+        fields = "__all__"
+        widgets = {
+            "nota_direcao_de_arte": forms.Select,
+            "nota_jogabilidade": forms.Select,
+            "nota_entretenimento": forms.Select,
+            "nota_imersao": forms.Select,
+        }
